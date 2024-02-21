5efb6e844b24b8b1d73a86b7d7c96614bbdc3a82
Francesc <fguerrero@linear.es>
2024-02-21 14:53:54 +0100

    Added tables from insert_maker

--- dev/sandbox.R
+++ dev/sandbox.R
@@ -8,6 +8,25 @@ con <-  dbConnect(drv)
 
 dplyr::tbl(con, "lot_changes") %>% dplyr::collect() %>% View()
 
+DBI::dbListTables(con)
+
+X <- dplyr::tbl(con, "supplier_lots") %>%
+  dplyr::filter(lot == "19087") %>%
+  dplyr::left_join(
+    dplyr::tbl(con, "lot_changes"),
+    dplyr::join_by(
+      id_supplier == new_id_supplier,
+      reference == new_reference,
+      lot == new_lot
+    )
+  ) %>%
+  dplyr::left_join(
+    dplyr::tbl(con, "lot_changes")
+  ) %>%
+  collect()
+
+
+
 # Exporting the database ----
 
 DBI::dbExecute(
@@ -21,3 +40,4 @@ DBI::dbExecute(
 
 # Disconnecting ----
 #DBI::dbDisconnect(con, shutdown = TRUE)
+
--- exported/schema.sql
+++ exported/schema.sql
@@ -2,68 +2,23 @@
 
 CREATE SEQUENCE id_sequence INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1 NO CYCLE;
 
-CREATE TABLE dict_units(
-  id_unit INTEGER PRIMARY KEY DEFAULT(nextval('id_sequence')),
-  unit VARCHAR
-  );
-
-CREATE TABLE sample_categories(
-  id_category INTEGER PRIMARY KEY DEFAULT(nextval('id_sequence')),
-  category VARCHAR
-);
-
-CREATE TABLE suppliers(
-  id_supplier INTEGER PRIMARY KEY DEFAULT(nextval('id_sequence')),
-  supplier VARCHAR
-);
-
-CREATE TABLE samples(
-  id_sample INTEGER PRIMARY KEY DEFAULT(nextval('id_sequence')),
-  id_category INTEGER,
-  FOREIGN KEY (id_category) REFERENCES sample_categories(id_category)
-  );
-
-CREATE TABLE supplier_references(
-  id_supplier INTEGER,
-  reference VARCHAR,
-  description VARCHAR,
-  FOREIGN KEY (id_supplier) REFERENCES suppliers(id_supplier),
-  PRIMARY KEY(id_supplier, reference)
-);
-
-CREATE TABLE supplier_lots(
-  id_sample INTEGER PRIMARY KEY,
-  id_supplier INTEGER,
-  reference VARCHAR,
-  lot VARCHAR,
-  date_manufacture DATE,
-  date_expiry DATE,
-  FOREIGN KEY (id_sample) REFERENCES samples(id_sample),
-  UNIQUE(id_supplier, reference, lot),
-  FOREIGN KEY (id_supplier, reference) REFERENCES supplier_references(id_supplier, reference)
-);
-
-CREATE TABLE sample_mixtures(
-  id_sample INTEGER PRIMARY KEY,
-  date DATE,
-  FOREIGN KEY (id_sample) REFERENCES samples(id_sample)
-);
-
-CREATE TABLE mixture_components(
-  id_sample INTEGER,
-  id_component INTEGER,
-  quantity FLOAT,
-  id_unit INTEGER,
-  FOREIGN KEY (id_sample) REFERENCES sample_mixtures(id_sample),
-  FOREIGN KEY (id_component) REFERENCES samples(id_sample),
-  FOREIGN KEY (id_unit) REFERENCES dict_units(id_unit)
-);
-
-CREATE TABLE sample_others(
-  id_sample INTEGER PRIMARY KEY,
-  description VARCHAR,
-  FOREIGN KEY (id_sample) REFERENCES samples(id_sample)
-);
+CREATE TABLE analytes(analyte VARCHAR PRIMARY KEY);
+CREATE TABLE suppliers(id_supplier INTEGER PRIMARY KEY DEFAULT(nextval('id_sequence')), supplier VARCHAR NOT NULL UNIQUE);
+CREATE TABLE sample_categories(id_category INTEGER PRIMARY KEY DEFAULT(nextval('id_sequence')), category VARCHAR NOT NULL UNIQUE);
+CREATE TABLE dict_units(id_unit INTEGER PRIMARY KEY DEFAULT(nextval('id_sequence')), unit VARCHAR NOT NULL UNIQUE);
+CREATE TABLE alt_temp(parameter VARCHAR PRIMARY KEY, f_t1_t2 FLOAT, f_t1_t3 FLOAT);
+CREATE TABLE supplier_references(id_supplier INTEGER, reference VARCHAR, description VARCHAR, FOREIGN KEY (id_supplier) REFERENCES suppliers(id_supplier), PRIMARY KEY(id_supplier, reference));
+CREATE TABLE samples(id_sample INTEGER PRIMARY KEY DEFAULT(nextval('id_sequence')), id_category INTEGER, FOREIGN KEY (id_category) REFERENCES sample_categories(id_category));
+CREATE TABLE alt_units(analyte VARCHAR PRIMARY KEY, alt_units VARCHAR, f_alt_units FLOAT, FOREIGN KEY (analyte) REFERENCES analytes(analyte));
+CREATE TABLE esp(analyte VARCHAR PRIMARY KEY, spanish VARCHAR, FOREIGN KEY (analyte) REFERENCES analytes(analyte));
+CREATE TABLE methods(parameter VARCHAR PRIMARY KEY, analyte VARCHAR, "method" VARCHAR, f_sd_cn FLOAT NOT NULL, f_sd_ca FLOAT NOT NULL, units VARCHAR NOT NULL, FOREIGN KEY (analyte) REFERENCES analytes(analyte), FOREIGN KEY (analyte) REFERENCES analytes(analyte));
+CREATE TABLE sample_versions(id_sample INTEGER, "version" INTEGER, date_version DATE, PRIMARY KEY(id_sample, "version"), FOREIGN KEY (id_sample) REFERENCES samples(id_sample));
+CREATE TABLE sample_values(id_sample INTEGER, "version" INTEGER, parameter VARCHAR, "value" FLOAT, PRIMARY KEY(id_sample, "version", parameter), FOREIGN KEY (id_sample, "version") REFERENCES sample_versions(id_sample, "version"), FOREIGN KEY (parameter) REFERENCES methods(parameter));
+CREATE TABLE sample_others(id_sample INTEGER PRIMARY KEY, description VARCHAR NOT NULL, FOREIGN KEY (id_sample) REFERENCES samples(id_sample));
+CREATE TABLE sample_mixtures(id_sample INTEGER PRIMARY KEY, date DATE, FOREIGN KEY (id_sample) REFERENCES samples(id_sample));
+CREATE TABLE supplier_lots(id_sample INTEGER PRIMARY KEY, id_supplier INTEGER, reference VARCHAR, lot VARCHAR, date_manufacture DATE, date_expiry DATE, FOREIGN KEY (id_sample) REFERENCES samples(id_sample), UNIQUE(id_supplier, reference, lot), FOREIGN KEY (id_supplier, reference) REFERENCES supplier_references(id_supplier, reference));
+CREATE TABLE lot_changes(original_id_supplier INTEGER, original_reference VARCHAR, original_lot VARCHAR, new_id_supplier INTEGER, new_reference VARCHAR, new_lot VARCHAR, FOREIGN KEY (original_id_supplier, original_reference, original_lot) REFERENCES supplier_lots(id_supplier, reference, lot), FOREIGN KEY (new_id_supplier, new_reference, new_lot) REFERENCES supplier_lots(id_supplier, reference, lot));
+CREATE TABLE mixture_components(id_sample INTEGER, id_component INTEGER, quantity FLOAT NOT NULL, id_unit INTEGER, FOREIGN KEY (id_sample) REFERENCES sample_mixtures(id_sample), FOREIGN KEY (id_component) REFERENCES samples(id_sample), FOREIGN KEY (id_unit) REFERENCES dict_units(id_unit));
 
 
 
